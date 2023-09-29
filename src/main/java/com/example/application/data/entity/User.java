48249885115c33fc817e50d1e521b1be7e406f78
Sidney Williams <101272072+slw723@users.noreply.github.com>
2023-09-28 19:36:51 -0500

    Updated User entity

--- src/main/java/com/example/application/data/entity/User.java
+++ src/main/java/com/example/application/data/entity/User.java
@@ -10,18 +10,62 @@ import java.util.List;
 @Entity
 public class User extends Appointment {
     @NotBlank
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int phoneNumber;
 
     @OneToMany(mappedBy = "company")
     @Nullable
     private List<ServiceProvider> employees = new LinkedList<>();
 
-    public String getName() {
-        return name;
+    public User (String first, String last, String emailAddress, String pw, int phone) {
+        firstName = first;
+        lastName = last;
+        email = emailAddress;
+        password = pw;
+        phoneNumber = phone;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String first) {
+        firstName = first;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String last) {
+        lastName = last;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String emailAddress) {
+        email = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String pw) {
+        password = pw;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phone) {
+        phoneNumber = phone;
     }
 
     public List<ServiceProvider> getEmployees() {
